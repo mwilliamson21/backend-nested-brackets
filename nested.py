5d162bda866c50a978afd48a4f54877910fd1e14
Marsha Williamson <tramar92@hotmail.com>
2019-09-06 11:38:33 -0400

    second commit with output.txt file corrected

--- nested.py
+++ nested.py
@@ -6,46 +6,49 @@ Module docstring: One line description of what your program does.
 __author__ = "MWilliamson21 with help from instructor"
 
 import sys
-openers = ['[', '(', '{', '(*', '<']
-closers = [']', ')', '}', '*)', '>']
 
-
-def is_nested(s):
-    stack = []
-    for n in s:
-        if n in openers:
-            stack.append(n)
-        if n in closers:  # for example, `}`
-            matching_opener = openers[closers.index(n)]
-            if len(stack) == 0 or stack.pop() != matching_opener:
-                return "NO "
-    if stack:  # tests if stack is empty or not
-        return "NO "
-    return "Yes "
-
-
-def main():
-    with open('input.txt') as infile:
-        for line in infile:
-            result = is_nested(line)
-            print(result)
-            # print(line)
-    pass  # Guaranteed now that k gets closed automatically
-    # k.close()
-
-
-if __name__ == '__main__':
-    main()
-
+# declare two sets of lists with matching opener/closer tokens
+# They must be lined up by index, because we will cross-index between them.
+open_li = ["[", "(", "{", "(*", "<"]
+closed_li = ["]", ")", "}", "*)", ">"]
 
 def valid_parentheses(string):
     stack = []
-    for n in string:
-        if n == "(":
-            stack.append(n)
-        if n == ")":
-            if len(stack) == 0 or stack.pop() != "(":
-                return False
-    if len(stack) != 0:
-        return False
-    return True
+    c = 0
+    while string:
+        token = string[0]  # look at first char of string as token
+        # handle 'eyebrows'
+        if string.startswith('(*'):
+            # eyebrow opener
+            token = '(*'
+        elif string.startswith('*)'):
+            # eyebrow closer
+            token = '*)'
+        c = c+1
+        if token in open_li:
+            stack.append(token)
+        elif token in closed_li:
+            # we have a closing token.  Find its opener buddy
+            closer_index = closed_li.index(token)
+            matching_opener = open_li[closer_index]
+            # if things are balanced, the matching opener should be at top of stack.
+            # Test if stack is empty, or is not balanced.
+            if len(stack) == 0 or matching_opener != stack.pop():
+                # early exit from while loop
+                return "NO " + str(c)
+        string = string[len(token):]
+    # Final check after while loop is done.
+    # If there is anything left on stack, that is unbalanced.    
+    if stack:
+        return "NO " + str(c)
+    else:
+        return "YES"
+def main():
+    with open("input.txt", "r") as file_open:
+        with open("output.txt", "a") as output:
+            for row in file_open:
+                results = valid_parentheses(row)
+                print(results)
+                output.write(results + "\n")
+if __name__ == '__main__':
+    main()
